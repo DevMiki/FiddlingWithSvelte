9deec3d1bd4f620b83cae411184dbf69d9d26220
CodeBrothers <ytcodebrothers@gmail.com>
2025-05-04 10:51:43 +0200

    :DEV: initial commit

---
+++ Pack_BE/src/main/java/org/example/pack_be/PackBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.pack_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackBeApplication.class, args);
+    }
+
+}
---
+++ Pack_BE/src/test/java/org/example/pack_be/PackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pack_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
